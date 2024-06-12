a7767283d3192661df36a1c6f48eccc73df4af5c
Sulaiman <soliman@gmail.com>
2024-06-12 23:36:45 +0430

    "Moved middleware 'auth:sanctum' to separate line and indented group function accordingly."

--- routes/api.php
+++ routes/api.php
@@ -7,10 +7,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::name('api')
-->middleware('auth:sanctum')->group(function () {
-    Route::get('/user', fn(Request $request) => CardInfoResource::make(CardInfo::first()));
-    Route::controller(CheckEmployeeInfo::class)
-        ->group(function () {
-            Route::get("employee/check", 'check')->name('employee.check');
-        });
-});
+    ->middleware('auth:sanctum')
+    ->group(function () {
+        Route::get('/user', fn(Request $request) => CardInfoResource::make(CardInfo::first()));
+        Route::controller(CheckEmployeeInfo::class)
+            ->group(function () {
+                Route::get("employee/check", 'check')->name('employee.check');
+            });
+    });

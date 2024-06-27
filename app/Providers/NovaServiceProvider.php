b66176b51f77dc13503299b577600d1b5a9ab4ef
Sulaiman <soliman@gmail.com>
2024-06-27 14:23:26 +0430

    Remove Armor Models

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use App\Nova\Card\ArmorVehicalCard;
-use App\Nova\Card\BlackMirrorVehicalCard;
 use App\Nova\Card\CardInfo as CardCardInfo;
 use App\Nova\Card\EmployeeVehicalCard;
 use App\Nova\Card\GunCard;

285ec67d9871dfb64882cedf88c2e1bb1d6ff637
Sulaiman <soliman@gmail.com>
2024-09-13 11:53:24 +0430

    Print/Design Card Package UPdated

--- package/sq/Card/routes/web.php
+++ package/sq/Card/routes/web.php
@@ -7,16 +7,16 @@ use Sq\Card\Livewire\CardDesign;
 Route::middleware(['auth'])->group(function () {
 
     // Card Frame Design Request Route
-    Route::middleware('role:Design Card')->get('card-design/{printCardFrame:id}', CardDesign::class)->name('employee.design-card');
+    Route::middleware('permission:design-card')->get('card-design/{printCardFrame:id}', CardDesign::class)->name('employee.design-card');
 
     // Card Frame Print Request Route
 
     // // Employee Card
-    Route::middleware('role:Print Card')->get('print-employee-card-for/{cardInfo:id}/card/{printCardFrame}', (new PrintCardController())->employee(...))->name('employee.print-card-for');
+    Route::middleware('permission:print-card')->get('print-employee-card-for/{cardInfo:id}/card/{printCardFrame}', (new PrintCardController())->employee(...))->name('employee.print-card-for');
 
     // Gun Card
-    Route::middleware('role:Print Card')->get('print/gun/{cardInfo:id}/card/{printCardFrame}', (new PrintCardController())->gun(...))->name('gun.print-card-for');
+    Route::middleware('permission:print-card')->get('print/gun/{cardInfo:id}/card/{printCardFrame}', (new PrintCardController())->gun(...))->name('gun.print-card-for');
 
     // Employee Car Card
-    Route::middleware('role:Print Card')->get('print/employeeCar/{employeeVehicalCard:id}/card/{printCardFrame}', (new PrintCardController())->employee_car(...))->name('employee-car.print-card-for');
+    Route::middleware('permission:print-card')->get('print/employeeCar/{employeeVehicalCard:id}/card/{printCardFrame}', (new PrintCardController())->employee_car(...))->name('employee-car.print-card-for');
 });
--- package/sq/Card/src/Http/Controllers/PrintCardController.php
+++ package/sq/Card/src/Http/Controllers/PrintCardController.php
@@ -10,11 +10,15 @@ use App\Support\Defense\Print\PrintTypeEnum;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 use Sq\Card\Support\PrintCardField;
+use Sq\Query\Policy\UserDepartment;
 
 class PrintCardController
 {
     public function employee(Request $request, CardInfo $cardInfo, int $printCardFrame): View
     {
+        if (!in_array($cardInfo->department_id, UserDepartment::getUserDepartment())) {
+            abort(404);
+        }
         $card = PrintCardFrame::findOrFail($printCardFrame);
         if (!$card->type == PrintTypeEnum::Employee) {
             abort(404);
@@ -29,27 +33,42 @@ class PrintCardController
     }
     public function gun(Request $request, CardInfo $cardInfo, int $printCardFrame): View
     {
+
+        if (!in_array($cardInfo->department_id, UserDepartment::getUserDepartment())) {
+            abort(404);
+        }
+
         $card = PrintCardFrame::findOrFail($printCardFrame);
         if (!$card->type == PrintTypeEnum::Gun) {
             abort(404);
         }
         app()->setLocale('fa');
+
         // Get / Replace the field to Value
         $field = new PrintCardField($cardInfo, $card);
         $details = $field->details;
         $remark = $field->remark;
+
         return view(($card->dim === "vertical") ? 'sqcard::print.card-vertical' : 'sqcard::print.card-horizontal', compact('cardInfo', 'card', 'details', 'remark'));
 
     }
 
     public function employee_car(Request $request, EmployeeVehicalCard $employeeVehicalCard, int $printCardFrame): View
     {
+
+        if (!in_array($employeeVehicalCard->card_info->department_id, UserDepartment::getUserDepartment())) {
+            abort(404);
+        }
+
+
         $card = PrintCardFrame::findOrFail($printCardFrame);
+
         if (!$card->type == PrintTypeEnum::EmployeeCar) {
             abort(404);
         }
 
         app()->setLocale('fa');
+
         // Get / Replace the field to Value
         $field = new PrintCardField($employeeVehicalCard->card_info, $card, $employeeVehicalCard);
         $details = $field->details;

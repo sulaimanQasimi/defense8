b3f0e811b4f55255989de6f1b8671729d01249b7
Sulaiman <farzadqasimy@gmail.com>
2024-10-11 00:05:26 +0430

    Update attendance.blade.php

--- package/sq/Employee/resources/views/employee/attendance.blade.php
+++ package/sq/Employee/resources/views/employee/attendance.blade.php
@@ -1,22 +1,17 @@
 <div class="flex justify-around mt-10">
 
     {{--  IF Current Gate is Main Gate And Enter Gate is empty --}}
-    @if (in_array($employee?->gate?->id, \Sq\Query\Policy\UserDepartment::getUserGate()))
-        @if ($date['start'])
-            @if ($attendance['present'])
-                <a href="{{ route('sqemployee.employee.check.pass', ['cardInfo' => $employee->id, 'state' => 'enter']) }}"
-                    class="px-7 rounded-lg hover:scale-95 py-1 text-white bg-gradient-to-t from-green-600 to-green-500"
-                    style="">@lang('Present')
-                </a>
-            @endif
+    @if ($attendance['allowed_gate'])
+        @if ($date['start'] && $attendance['present'])
+            <a href="{{ route('sqemployee.employee.check.pass', ['cardInfo' => $employee->id, 'state' => 'enter']) }}"
+                class="px-7 rounded-lg hover:scale-95 py-1 text-white bg-gradient-to-t from-green-600 to-green-500"
+                style="">@lang('Present')
+            </a>
         @endif
-        @if ($date['end'])
-            @if ($attendance['exit'])
-                <a href="{{ route('sqemployee.employee.check.pass', ['cardInfo' => $employee->id, 'state' => 'exit']) }}"
-                    class="px-7 rounded-lg hover:scale-95 py-1 text-white bg-gradient-to-t from-red-600 to-red-500"
-                    style="">@lang('Exited')</a>
-            @endif
-
+        @if ($date['end'] && $attendance['exit'])
+            <a href="{{ route('sqemployee.employee.check.pass', ['cardInfo' => $employee->id, 'state' => 'exit']) }}"
+                class="px-7 rounded-lg hover:scale-95 py-1 text-white bg-gradient-to-t from-red-600 to-red-500"
+                style="">@lang('Exited')</a>
         @endif
 
         @if ($attendance['absent'])

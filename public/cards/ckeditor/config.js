718e713d4dacdeb2af37bffcc0b34448e1085c23
mohammadrafi10 <mohammadrafishirzai83@gmail.com>
2024-10-12 12:01:20 +0530

    font faces added
    the required text fonts uploaded successfully.

--- public/cards/ckeditor/config.js
+++ public/cards/ckeditor/config.js
@@ -13,21 +13,61 @@
 CKEDITOR.editorConfig = function (config) {
     // config.extraPlugins = 'richcombo';
 
-    config.uiColor = '#AADC6E';
+    config.uiColor = "#AADC6E";
 
-    config.language = 'fa';
-    config.extraPlugins = 'lineheight';
-    config.line_height="0.1em;0.2em;0.3em;0.4em;0.5em;0.6em;0.7em;0.8em;0.9em;1em;1.1em;1.2em;1.3em;1.4em;1.5em";
+    config.language = "fa";
+    config.extraPlugins = "lineheight";
+    config.line_height =
+        "0.1em;0.2em;0.3em;0.4em;0.5em;0.6em;0.7em;0.8em;0.9em;1em;1.1em;1.2em;1.3em;1.4em;1.5em";
     config.toolbar = [
-        { name: 'basicstyles', items: ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'CopyFormatting', 'RemoveFormat'] },
-        { name: 'paragraph', items: ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl', 'Language'] },
-        { name: 'links', items: ['Link', 'Unlink', 'Anchor'] },
-        { name: 'insert', items: ['HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak'] },
-        { name: 'styles', items: ['Styles', 'Format', 'Font', 'FontSize'] },
-        { name: 'colors', items: ['TextColor', 'BGColor'] },
-        { name: 'tools', items: ['Maximize', 'ShowBlocks','lineheight'] },
+        {
+            name: "basicstyles",
+            items: [
+                "Bold",
+                "Italic",
+                "Underline",
+                "Strike",
+                "Subscript",
+                "Superscript",
+                "-",
+                "CopyFormatting",
+                "RemoveFormat",
+            ],
+        },
+        {
+            name: "paragraph",
+            items: [
+                "NumberedList",
+                "BulletedList",
+                "-",
+                "Outdent",
+                "Indent",
+                "-",
+                "Blockquote",
+                "CreateDiv",
+                "-",
+                "JustifyLeft",
+                "JustifyCenter",
+                "JustifyRight",
+                "JustifyBlock",
+                "-",
+                "BidiLtr",
+                "BidiRtl",
+                "Language",
+            ],
+        },
+        { name: "links", items: ["Link", "Unlink", "Anchor"] },
+        {
+            name: "insert",
+            items: ["HorizontalRule", "Smiley", "SpecialChar", "PageBreak"],
+        },
+        { name: "styles", items: ["Styles", "Format", "Font", "FontSize"] },
+        { name: "colors", items: ["TextColor", "BGColor"] },
+        { name: "tools", items: ["Maximize", "ShowBlocks", "lineheight"] },
     ];
 
     config.contentCss = "../font.css";
-    config.font_names = 'nasim/persian-font;nazanin/nazanin-font' + config.font_names;
+    config.font_names =
+        "nasim/persian-font;nazanin/nazanin-font;ihsan-bv/ihsan-bv;ihsan-likedood/ihsan-likedood;ihsan-MV/ihsan-MV;ihsan-NV/ihsan-NV;" +
+        config.font_names;
 };

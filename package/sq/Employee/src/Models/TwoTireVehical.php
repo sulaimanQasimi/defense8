e0e3ab01d8a8ae57aa48f66ae603f3543b5c7bea
SulaimanQasimi <farzadqasimy@gmail.com>
2025-01-25 13:35:09 +0430

    `Update database and code for department and user permissions`

---
+++ package/sq/Employee/src/Models/TwoTireVehical.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Sq\Employee\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class TwoTireVehical extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+}

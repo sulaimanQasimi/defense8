beb55a744d9d143d5a2d27c4559a5c5ea557ba8a
SulaimanQasimi <farzadqasimy@gmail.com>
2025-02-15 15:07:30 +0430

    `Added Spatie Activitylog functionality to EmployeeVehicalCard model`

--- package/sq/Employee/src/Models/EmployeeVehicalCard.php
+++ package/sq/Employee/src/Models/EmployeeVehicalCard.php
@@ -7,6 +7,8 @@ use App\Support\HasDriver;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class EmployeeVehicalCard extends Model
 {
@@ -14,12 +16,22 @@ class EmployeeVehicalCard extends Model
     use SoftDeletes;
     use HasCardInfo;
     use HasDriver;
+
+    use LogsActivity;
     protected $fillable = ['remark','register_date','expire_date','printed'];
     protected $casts = [
         'register_date' => "date",
         'expire_date' => "date",
         'printed'=>'boolean',
     ];
+
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()->logAll()
+            ->setDescriptionForEvent(fn(string $eventName) => "This model has been {$eventName}");
+    }
+
 
 
 }

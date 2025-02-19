db79a8de3366d60f4d786a1e55618767f496637b
SulaimanQasimi <farzadqasimy@gmail.com>
2025-02-19 19:54:46 +0430

    `Update app.blade.php to include app.css and reorder script tags`

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,16 +1,21 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
-        <!-- Scripts -->
-        @routes
-        @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
-        @inertiaHead
-    </head>
-    <body class="font-sans antialiased">
-        @inertia
-    </body>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title inertia>{{ config('app.name', 'Laravel') }}</title>
+    <!-- Scripts -->
+    @routes
+
+    <link type="text/css" href="{{ asset('build/assets/app.css') }}" rel="stylesheet" />
+    @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
+    @inertiaHead
+</head>
+
+<body class="font-sans antialiased">
+    @inertia
+</body>
+
 </html>

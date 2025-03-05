586dbd6fac53e04f405525c00d2b82d739266e82
SulaimanQasimi <farzadqasimy@gmail.com>
2025-03-05 21:22:53 +0430

    `Added chartjs integration for print card metrics and data`

--- package/sq/Card/src/Nova/PrintCard.php
+++ package/sq/Card/src/Nova/PrintCard.php
@@ -5,6 +5,8 @@ namespace Sq\Card\Nova;
 use Afj95\LaravelNovaHijriDatepickerField\HijriDatePicker;
 use App\Nova\Resource;
 use App\Nova\User;
+use Coroowicaksono\ChartJsIntegration\LineChart;
+use Coroowicaksono\ChartJsIntegration\ScatterChart;
 use Illuminate\Http\Request;
 use Laravel\Nova\Fields\BelongsTo;
 use Laravel\Nova\Fields\ID;
@@ -66,9 +68,56 @@ class PrintCard extends Resource
     }
     public function cards(NovaRequest $request)
     {
+        $currentYear = verta()->year;
+        $startDate = verta()->startYear()->datetime();
+        $endDate = verta()->endYear()->datetime();
+
+        $printData = static::$model::query()
+            ->whereBetween('created_at', [$startDate, $endDate])
+            ->get()
+            ->groupBy(function ($item) {
+                $v = verta($item->created_at);
+                return $v->format('n'); // Get month number (1-12)
+            })
+            ->map(function ($group) {
+                return $group->count();
+            })
+            ->sortKeys(); // Sort by month number
+
         return [
             new Metrics\PrintCardMetric(),
             new Metrics\PrintCardFrameMetric(),
+            (new LineChart)
+                ->title(trans('جدول کارت های پرنت شده') . $currentYear)
+                ->series([
+                    [
+                        'label' => trans('کارت های پرنت شده'),
+                        'backgroundColor' => '#4099DE',
+                        'borderColor' => '#4099DE',
+                        'fill' => false,
+                        'data' => $printData->values()->toArray()
+                    ]
+                ])
+                ->options([
+                    'responsive' => true,
+                    'maintainAspectRatio' => false,
+                    'xaxis' => ['categories' => $printData->keys()->toArray()],
+                    'scales' => [
+                        'y' => [
+                            'beginAtZero' => true,
+                            'title' => [
+                                'display' => true,
+                                'text' => trans('تعداد کارت')
+                            ]
+                        ],
+                        'x' => [
+                            'title' => [
+                                'display' => true,
+                                'text' => trans('ماه')
+                            ]
+                        ]
+                    ]
+                ])
         ];
     }
     public function filters(NovaRequest $request)

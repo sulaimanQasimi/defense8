430ad12f7c830da112a6b580beaa5b1d3442327d
SulaimanQasimi <farzadqasimy@gmail.com>
2025-03-05 20:29:27 +0430

    `Refactor PrintCardMetric.php: add icon, update translations and commented out code`

--- package/sq/Card/src/Nova/Metrics/PrintCardMetric.php
+++ package/sq/Card/src/Nova/Metrics/PrintCardMetric.php
@@ -6,29 +6,33 @@ use Laravel\Nova\Http\Requests\NovaRequest;
 use Laravel\Nova\Metrics\Value;
 use Sq\Card\Models\PrintCard;
 use Sq\Query\Policy\UserDepartment;
+use Laravel\Nova\WithIcon;
 
 class PrintCardMetric extends Value
 {
+    public $icon= 'fa-solid fa-id-card';
+
+
     public function name()
     {
-        return __('Print Card Count');
+        return __('تعداد کارت های چاپ شده');
     }
 
     public function calculate(NovaRequest $request)
     {
         $query = PrintCard::query();
 
-        if (!auth()->user()->hasRole('super-admin')) {
-            $query->whereHas('card_info', function ($query) {
-                return $query->whereIn('department_id', UserDepartment::getUserDepartment());
-            });
-        }
+        // if (!auth()->user()->hasRole('super-admin')) {
+        //     $query->whereHas('card_info', function ($query) {
+        //         return $query->whereIn('department_id', UserDepartment::getUserDepartment());
+        //     });
+        // }
 
-        if ($request->has('department_id')) {
-            $query->whereHas('card_info', function ($query) use ($request) {
-                return $query->where('department_id', $request->department_id);
-            });
-        }
+        // if ($request->has('department_id')) {
+        //     $query->whereHas('card_info', function ($query) use ($request) {
+        //         return $query->where('department_id', $request->department_id);
+        //     });
+        // }
 
         return $this->count($request, $query);
     }
@@ -38,11 +42,9 @@ class PrintCardMetric extends Value
         return [
             30 => __('30 Days'),
             60 => __('60 Days'),
-            365 => __('365 Days'),
-            'TODAY' => __('Today'),
-            'MTD' => __('Month To Date'),
-            'QTD' => __('Quarter To Date'),
-            'YTD' => __('Year To Date'),
+            365 => __('امسال'),
+            'TODAY' => __('امروز'),
+            "ALL"=> __('همه'),
         ];
     }
 
